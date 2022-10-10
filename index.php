44efb76a17267242b00b1c30b041059db307cc00
Aiyappa <aiyappa722@gmail.com>
2022-10-10 14:52:17 +0530

    a

--- index.php
+++ index.php
@@ -1,4 +1,1 @@
-<?php include_once("theme\index.html"); ?>
-<?php include_once("theme\about.html"); ?>
-<?php include_once("theme\services.html"); ?>
-<?php include_once("theme\contact.html"); ?>
+<?php include_once("theme/index.html"); ?>
